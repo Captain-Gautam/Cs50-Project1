9a730274fda6aa108edfae90c7b8f2e3ef6fcd29
Captain-Gautam <gautamprajapati2252@gmail.com>
2023-04-09 10:16:01 +0530

    Files

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from markdown2 import Markdown
+import markdown
 
 from . import util
+
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
+    
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+
+def entry(request, title):
+    return
 
